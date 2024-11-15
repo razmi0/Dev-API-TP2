8cd9f61299cc3e3645ddd3eea74df2dbdb974ae0
razmiO <thomas.cuesta31@gmail.com>
2024-11-15 12:17:17 +0100

    Refactor : Replace all business Error related code by a FALSE early return, add offset on findAll and

--- model/dao/ProductDao.php
+++ model/dao/ProductDao.php
@@ -2,22 +2,20 @@
 
 namespace API\Model\Dao;
 
-use Exception;
-use API\Model\{Entity\Product, Dao\Connection};
-use API\HTTP\Error as Error;
 use PDO;
+use API\Model\{Entity\Product, Dao\Connection};
 
 
 /**
  * Class ProductDao
  * 
- * Used to interact with the database and the T_PRODUIT table
+ * Used to interact with a db and a table
  * 
  * @property PDO $pdo
  * @property Connection $connection
  * 
  * @method create(Product $produit)
- * @method findAll(int $limit = null)
+ * @method findAll(int $limit = null, int offset = 0, string $direction = "DESC")
  * @method findById(int $id)
  * @method deleteById(int $id)
  * @method update(Product $produit)
@@ -29,323 +27,252 @@ class ProductDao
     private ?PDO $pdo = null;
     private ?Connection $connection = null;
 
-    public function __construct()
+    public function __construct(Connection $connection)
     {
-
-        $this->connection = new Connection();
+        $this->connection = $connection;
         $this->pdo = $this->connection->getPDO();
     }
 
     /**
-     * @param Product $produit
-     * @throws Error 
-     * @return string $insertedId
+     * Create a new product in the database
+     * @return int The id of the last inserted product in the database
      */
-    public function create($produit)
+    public function create(Product $produit): int
     {
-        try {
-            // Setup the error message
+        // Build the query
+        $query = "INSERT INTO " . $this->connection->getTableName() . " (name, description, prix, date_creation)";
+        $query .= " VALUES (:name, :description, :prix, :date_creation)";
 
-            // Build the query
-            $query = "INSERT INTO T_PRODUIT (name, description, prix, date_creation)";
-            $query .= " VALUES (:name, :description, :prix, :date_creation)";
+        // Verify the preparation of the query
+        $prepared = $this->pdo->prepare($query);
 
-            // Verify the preparation of the query
-            $prepared = $this->pdo->prepare($query);
-            if (!$prepared) {
-                Error::HTTP500("Erreur interne");
-            }
+        // Bind the parameters
+        $id = $produit->getId();
+        $name = $produit->getProductName();
+        $description = $produit->getDescription();
+        $prix = $produit->getPrix();
+        $date_creation = $produit->getDateCreation();
 
-            // Bind the parameters
-            $id = $produit->getId();
-            $name = $produit->getProductName();
-            $description = $produit->getDescription();
-            $prix = $produit->getPrix();
-            $date_creation = $produit->getDateCreation();
-
-            if (!is_null($id))
-                $prepared->bindParam(':id', $id, PDO::PARAM_INT);
-
-            // No PARAM_* default to PARAM_STR
-            if (!is_null($name))
-                $prepared->bindParam(':name', $name);
-
-            if (!is_null($description))
-                $prepared->bindParam(':description', $description);
-
-            // we use bindValue here because we want to keep the right type (double) for the price
-            if (!is_null($prix))
-                $prepared->bindValue(':prix', $prix);
-
-            if (!is_null($date_creation))
-                $prepared->bindParam(':date_creation', $date_creation);
-
-            // Verify the execution of the query
-            $stmt = $prepared->execute();
-            if (!$stmt) {
-                Error::HTTP500("Erreur interne");
-            }
-
-            // If all went good, we will return the id of the last inserted product in db to the controller
-            return $this->pdo->lastInsertId();
-        } catch (Error $e) {
-            // If an error was catch, we send an informative error message back to the controller
-            throw $e;
-        }
-        $this->connection->closeConnection();
-    }
-
-
-    /**
-     * 
-     * @description Find all products
-     * @param int $limit
-     * @throws Error
-     * @return Product[]
-     * 
-     */
-    public function findAll(int $limit = null): array
-    {
-
-        try {
-
-            // Build the query
-            $query = "SELECT * FROM " . $this->connection->getTableName() . " ORDER BY date_creation DESC";
-            $query .= $limit ? " LIMIT :limit" : "";
-
-            // Verify the preparation of the query
-            $prepared = $this->pdo->prepare($query);
-            if (!$prepared) {
-                Error::HTTP500("Erreur interne");
-            }
-
-            // Bind the parameters
-            if ($limit) {
-                $prepared->bindParam(':limit', $limit, PDO::PARAM_INT);
-            }
-
-            // Verify the execution of the query
-            $stmt = $prepared->execute();
-            if (!$stmt) {
-                Error::HTTP500("Erreur interne");
-            }
-            $products_from_db = $prepared->fetchAll();
-
-            // If no product was found, we send a response with a 404 status code and an error message
-            if (count($products_from_db) == 0) {
-                Error::HTTP404("Aucun produit trouvé");
-            }
-
-            // We map the products from the database to a new array of products entities and return it to the controller
-            return array_map(fn($product) => Product::make($product), $products_from_db);
-        } catch (Error $e) {
-            // If an error was catch, we send a response with a 500 status code and an error message
-            throw $e;
-        }
-        $this->connection->closeConnection();
-    }
-
-
-    /**
-     * 
-     * @param int $id
-     * @throws Error
-     * @return Product
-     * 
-     */
-    public function findById(int $id): Product
-    {
-        try {
-
-            // Build the query
-            $query = "SELECT * FROM " . $this->connection->getTableName() . " WHERE id = :id";
-
-            // Verify the preparation of the query
-            $prepared = $this->pdo->prepare($query);
-            if (!$prepared) {
-                Error::HTTP500("Erreur interne");
-            }
-
-            // Bind the parameters
+        if (!is_null($id))
             $prepared->bindParam(':id', $id, PDO::PARAM_INT);
 
-            // Verify the execution of the query
-            $stmt = $prepared->execute();
-            if (!$stmt) {
-                Error::HTTP500("Erreur interne");
-            }
+        if (!is_null($name))
+            $prepared->bindParam(':name', $name);
 
-            // Fetch the result
-            $result = $prepared->fetch();
+        if (!is_null($description))
+            $prepared->bindParam(':description', $description);
 
-            // If no product was found, we send a response with a 404 status code and an error message
-            if (!$result) {
-                Error::HTTP404("Aucun produit trouvé", ["id" => $id]);
-            }
+        if (!is_null($prix))
+            $prepared->bindValue(':prix', $prix);
 
-            // Create a new product object and return it to the controller
-            return Product::make($result);
-        } catch (Error $e) {
-            // If an error was catch, we send a response with a 500 status code and an error message
-            throw $e;
-        }
+        if (!is_null($date_creation))
+            $prepared->bindParam(':date_creation', $date_creation);
+
+        // Execute the query
+        $prepared->execute();
+
         $this->connection->closeConnection();
+
+        // If all went good, we will return the id of the last inserted product in db to the controller
+        return (int)$this->pdo->lastInsertId();
     }
 
     /**
-     * 
-     * @description Delete a product by its id
-     * @param $id
-     * @throws Error
-     * @return int
-     * 
+     * Find all products and optionally order them by a column
      */
-    public function deleteById(int $id): int
+    public function findAll(int $limit = null, int $offset = 0, string $direction = "DESC"): array | false
     {
+        // Build the query
+        $query = "SELECT * FROM " . $this->connection->getTableName() . " ORDER BY date_creation " . $direction;
 
-        try {
-            // Build the query
-            $query = "DELETE FROM " . $this->connection->getTableName() . " WHERE id = :id";
-            // Verify the preparation of the query
-            $prepared = $this->pdo->prepare($query);
-            if (!$prepared) {
-                Error::HTTP500("Erreur interne");
-            }
+        // Add a limit to the query if it was provided
+        $query .= $limit ? " LIMIT :limit" : "";
 
-            // Bind the parameters
-            $prepared->bindParam(':id', $id, PDO::PARAM_INT);
+        // Add the offset to the query if it was provided
+        $query .= $offset ? " OFFSET :offset" : "";
 
-            // Verify the execution of the query
-            $stmt = $prepared->execute();
-            if (!$stmt) {
-                Error::HTTP500("Erreur interne");
-            }
+        // Verify the preparation of the query
+        $prepared = $this->pdo->prepare($query);
 
-            // Affected rows in the database
-            $affectedRows = $prepared->rowCount();
-
-            // return the number of affected rows
-            return $affectedRows;
-        } catch (Error $e) {
-            throw $e;
+        // Bind the limit if it was provided
+        if ($limit) {
+            $prepared->bindParam(':limit', $limit, PDO::PARAM_INT);
         }
-        $this->connection->closeConnection();
-    }
 
-
-    /**
-     * 
-     * @param Product $produit
-     * @throws Error
-     * @return array
-     * 
-     */
-    public function update(Product $produit): int
-    {
-
-        try {
-
-            // Get the id of the product
-            $id = $produit->getId();
-            $name = $produit->getProductName();
-            $description = $produit->getDescription();
-            $prix = $produit->getPrix();
-
-            // Build the query
-            $query = "UPDATE " . $this->connection->getTableName() . " SET ";
-
-            if (!empty($name))
-                $query .= "name = :name, ";
-
-            if (!empty($description))
-                $query .= "description = :description, ";
-
-            if (!empty($prix))
-                $query .= "prix = :prix ";
-
-            // Remove the last comma and space
-            $query = rtrim($query, ", ");
-            $query .= " WHERE id = :id";
-
-            // Verify the preparation of the query
-            $prepared = $this->pdo->prepare($query);
-            if (!$prepared) {
-                Error::HTTP500("Erreur interne", ["id" => $id]);
-            }
-
-            // Bind the parameters
-            $prepared->bindParam(':id', $id, PDO::PARAM_INT);
-
-            if (!empty($name))
-                $prepared->bindParam(':name', $name);
-
-            if (!empty($description))
-                $prepared->bindParam(':description', $description);
-
-            if (!empty($prix))
-                $prepared->bindValue(':prix', $prix);
-
-            // Verify the execution of the query
-            $stmt = $prepared->execute();
-            if (!$stmt) {
-                Error::HTTP500("Erreur interne", ["id" => $id]);
-            }
-
-            $affectedRows = $prepared->rowCount();
-
-            // return the number of affected rows
-            return $affectedRows;
-        } catch (Error $e) {
-            // If an error was catch, we send an informative error message back to the controller
-            throw $e;
+        // Bind the offset if it was provided
+        if ($offset) {
+            $prepared->bindParam(':offset', $offset, PDO::PARAM_INT);
         }
+
+        // Execute the query
+        $prepared->execute();
+
+        // Fetch the result
+        $products_from_db = $prepared->fetchAll();
+
         $this->connection->closeConnection();
+
+        // If no products were found, we return false
+        if (empty($products_from_db)) {
+            return false;
+        }
+
+        // We map the products from the database to a new array of products entities and return it to the controller
+        return Product::makeBulk($products_from_db);
     }
 
     /**
-     * 
-     * @param array $ids
-     * @throws Error
-     * @return Product[]
+     * Find a product by its id
+     * @return Product | false False if no product was found
+     */
+    public function findById(int $id): Product | false
+    {
+        // Build the query
+        $query = "SELECT * FROM " . $this->connection->getTableName() . " WHERE id = :id";
+
+        // Verify the preparation of the query
+        $prepared = $this->pdo->prepare($query);
+
+        // Bind the parameters
+        $prepared->bindParam(':id', $id, PDO::PARAM_INT);
+
+        // Execute the query
+        $prepared->execute();
+
+        // Fetch the result
+        $result = $prepared->fetch();
+
+        $this->connection->closeConnection();
+
+        if (empty($result)) {
+            return false;
+        }
+
+        // Create a new product object and return it to the controller
+        return Product::make($result);
+    }
+
+    /**
+     * Delete a product by its id
+     * @return int | false The number of affected rows or false if no row in the database was affected
+     */
+    public function deleteById(int $id): int | false
+    {
+        // Build the query
+        $query = "DELETE FROM " . $this->connection->getTableName() . " WHERE id = :id";
+
+        // Verify the preparation of the query
+        $prepared = $this->pdo->prepare($query);
+
+        // Bind the parameters
+        $prepared->bindParam(':id', $id, PDO::PARAM_INT);
+
+        // Execute the query
+        $prepared->execute();
+
+        // Affected rows in the database
+        $affectedRows = $prepared->rowCount();
+
+        $this->connection->closeConnection();
+
+        if ($affectedRows === 0) {
+            return false;
+        }
+
+        // return the number of affected rows
+        return $affectedRows;
+    }
+
+    /**
+     * Update a product in the database
+     * Query is built dynamically based on the data provided
+     * @return int | false The number of affected rows or false if no row in the database was affected
+     */
+    public function update(Product $produit): int | false
+    {
+        // Get the data from the product entity
+        $id = $produit->getId();
+        $name = $produit->getProductName();
+        $description = $produit->getDescription();
+        $prix = $produit->getPrix();
+
+        // Build the query
+        $query = "UPDATE " . $this->connection->getTableName() . " SET ";
+
+        if (!empty($name))
+            $query .= "name = :name, ";
+
+        if (!empty($description))
+            $query .= "description = :description, ";
+
+        if (!empty($prix))
+            $query .= "prix = :prix ";
+
+        // Remove the last comma and space
+        $query = rtrim($query, ", ");
+        $query .= " WHERE id = :id";
+
+        // Verify the preparation of the query
+        $prepared = $this->pdo->prepare($query);
+
+        // Bind the parameters
+        $prepared->bindParam(':id', $id, PDO::PARAM_INT);
+
+        if (!empty($name))
+            $prepared->bindParam(':name', $name);
+
+        if (!empty($description))
+            $prepared->bindParam(':description', $description);
+
+        if (!empty($prix))
+            $prepared->bindValue(':prix', $prix);
+
+        // Execute the query
+        $prepared->execute();
+
+        // Affected rows in the database
+        $affectedRows = $prepared->rowCount();
+
+        $this->connection->closeConnection();
+
+        if ($affectedRows === 0) {
+            return false;
+        }
+
+        // return the number of affected rows
+        return $affectedRows;
+    }
+
+    /**
+     * @return Product[] | false  False if no products were found 
      * 
      */
-    public function findManyById(array $ids): array
+    public function findManyById(array $ids): array | false
     {
-        try {
 
-            // Build the query
-            $query = "SELECT * FROM " . $this->connection->getTableName() . " WHERE id IN (";
+        // Build the query
+        $query = "SELECT * FROM " . $this->connection->getTableName() . " WHERE id IN (";
 
-            // We build the query with the number of ids
-            $query .= implode(",", array_fill(0, count($ids), "?"));
-            $query .= ")";
+        // We build the query with the number of ids
+        $query .= implode(",", array_fill(0, count($ids), "?"));
+        $query .= ")";
 
-            // Verify the preparation of the query
-            $prepared = $this->pdo->prepare($query);
-            if (!$prepared) {
-                Error::HTTP500("Erreur interne");
-            }
+        // Verify the preparation of the query
+        $prepared = $this->pdo->prepare($query);
 
-            // Verify the execution of the query
-            $stmt = $prepared->execute($ids);
-            if (!$stmt) {
-                Error::HTTP500("Erreur interne");
-            }
+        // Execute the query
+        $prepared->execute();
 
-            // Fetch the result
-            $result = $prepared->fetchAll();
+        // Fetch the result
+        $result = $prepared->fetchAll();
 
-            // If no product was found, we send a response with a 404 status code and an error message
-            if (!$result) {
-                Error::HTTP404("Aucun produit trouvé", ["ids" => $ids]);
-            }
-
-            // We map the products from the database to a new array of products entities and return it to the controller
-            return Product::makeBulk($result);
-        } catch (Exception $e) {
-            // If a not handled error was catch, we send a response with a 500 status code and an error message
-            throw $e->getMessage();
-        }
         $this->connection->closeConnection();
+
+        if (empty($result)) {
+            return false;
+        }
+
+        // We map the products from the database to a new array of products entities and return it to the controller
+        return Product::makeBulk($result);
     }
 }
