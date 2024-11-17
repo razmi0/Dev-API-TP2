2fe932ea214b43c0bde6642248d92a1c821749a3
razmiO <thomas.cuesta31@gmail.com>
2024-11-17 02:00:52 +0100

    Refactor Signup controller with DI configuration and new phprenderer templates integration

--- controllers/Signup.php
+++ controllers/Signup.php
@@ -6,6 +6,10 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\PhpRenderer;
 use Valitron\Validator;
+use API\Model\{
+    Entity\User,
+    Dao\UserDao
+};
 
 class Signup
 {
@@ -17,13 +21,21 @@ class Signup
         "confirm_password" => ["required", ["equals", "password"]]
     ];
 
-    public function __construct(private PhpRenderer $view, private Validator $validator) {}
+    const BASIC_VIEW_DATA = [
+        "title" => "Signup",
+    ];
+
+    public function __construct(
+        private PhpRenderer $view,
+        private Validator $validator,
+        private UserDao $userDao
+    ) {}
 
     public function new(Request $request, Response $response)
     {
 
         $viewData = [
-            "title" => "Signup",
+            ...self::BASIC_VIEW_DATA
         ];
 
         return $this->view->render($response, "signup.php", $viewData);
@@ -42,7 +54,7 @@ class Signup
 
             // send the data and the errors to the view
             return $this->view->render($response, "signup.php", [
-                "title" => "Signup",
+                ...self::BASIC_VIEW_DATA,
                 "errors" => $this->validator->errors(),
                 "data" => $data
             ]);
@@ -50,14 +62,24 @@ class Signup
 
         // if the data is valid, we start the registration process
 
-        // hasching password
+        // hashing password
         $data["password_hash"] = password_hash($data["password"], PASSWORD_DEFAULT);
 
+        // generate an api key
         $api_key = bin2hex(random_bytes(32));
 
+        // hashing the api key
         $data["api_key"] = $api_key;
 
-        print_r($data);
+        $data["api_key_hash"] = password_hash($api_key, PASSWORD_DEFAULT);
+
+        // Creating user entity
+        $user = User::make($data);
+
+        // Creating user dao
+        $insertedId = $this->userDao->create($user);
+
+        print_r($insertedId);
 
         return $response;
     }
